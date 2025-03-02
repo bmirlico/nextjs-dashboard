d9bf59d64929d3a990cbef05bcc355429ceb758c
Bastien Mirlicourtois <bmirlico@Mac-001.lan>
2025-03-02 15:27:30 +0100

    tutorial done

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,13 +1,17 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function seedUsers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-  await sql`
-    CREATE TABLE IF NOT EXISTS users (
+  try {
+    await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  } catch (error) {
+    console.error('Extension uuid-ossp already exists');
+  }
+    await sql`
+      CREATE TABLE IF NOT EXISTS users (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       email TEXT NOT NULL UNIQUE,
